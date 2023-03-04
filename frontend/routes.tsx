43c9d9a6f9d91b4edae6a99855e26a3fa852d3fd
start.vaadin.com <no-reply@vaadin.com>
2023-03-04 13:41:40 +0000

    Generated project

---
+++ frontend/routes.tsx
@@ -0,0 +1,39 @@
+import MainLayout from 'Frontend/views/MainLayout.js';
+import { lazy } from 'react';
+import { createBrowserRouter, IndexRouteObject, NonIndexRouteObject, useMatches } from 'react-router-dom';
+
+const TodoView = lazy(async () => import('Frontend/views/todo/TodoView.js'));
+export type MenuProps = Readonly<{
+  icon?: string;
+  title?: string;
+}>;
+
+export type ViewMeta = Readonly<{ handle?: MenuProps }>;
+
+type Override<T, E> = Omit<T, keyof E> & E;
+
+export type IndexViewRouteObject = Override<IndexRouteObject, ViewMeta>;
+export type NonIndexViewRouteObject = Override<
+  Override<NonIndexRouteObject, ViewMeta>,
+  {
+    children?: ViewRouteObject[];
+  }
+>;
+export type ViewRouteObject = IndexViewRouteObject | NonIndexViewRouteObject;
+
+type RouteMatch = ReturnType<typeof useMatches> extends (infer T)[] ? T : never;
+
+export type ViewRouteMatch = Readonly<Override<RouteMatch, ViewMeta>>;
+
+export const useViewMatches = useMatches as () => readonly ViewRouteMatch[];
+
+export const routes: readonly ViewRouteObject[] = [
+  {
+    element: <MainLayout />,
+    handle: { icon: 'null', title: 'Main' },
+    children: [{ path: '/', element: <TodoView />, handle: { icon: 'la la-list-alt', title: 'Todo' } }],
+  },
+];
+
+const router = createBrowserRouter([...routes]);
+export default router;
