8e7ad8dba5a2365f55e609b726b22c4802ef7cc0
David Sousa <ifini@pop-os-ifini.lan>
2023-03-09 12:04:58 +0000

    Starting CommandLiner Runner function

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -1,9 +1,13 @@
 package com.example.application;
 
+import com.example.application.model.Post;
+import com.example.application.repository.PostRepository;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.theme.Theme;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * The entry point of the Spring Boot application.
@@ -17,7 +21,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
+
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(PostRepository posts, UserRepository users) {
+        return args -> {
+            repository.save(new Post("Learn Spring Boot"));
+            repository.save(new Post("Learn Vaadin"));
+            repository.save(new Post("Learn Spring Data"));
+            repository.save(new Post("Learn Spring Security"));
+            repository.save(new Post("Learn Spring Cloud"));
+        };
     }
 
 }
