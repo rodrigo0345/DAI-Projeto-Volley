76b86d91f1d07fb44d00603a1af3d068db0ab107
Rodrigo Casanova <rodrigocralha@gmail.com>
2023-03-14 18:47:38 +0000

    forgot signup

--- frontend/views/DashboardView.tsx
+++ frontend/views/DashboardView.tsx
@@ -1,11 +1,43 @@
+import { signup } from 'Frontend/generated/AuthenticationController';
+import { useRef } from 'react';
+
 export default function DashboardView() {
+  const email = useRef<HTMLInputElement>(null);
+  const password = useRef<HTMLInputElement>(null);
+
+  function onSubmit() {
+    const emailValue = email.current?.value;
+    const passwordValue = password.current?.value;
+
+    try {
+      signup({ email: emailValue, password: passwordValue });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className='min-h-screen'>
-      <div className='py-44'>
-        <input type='text' className='bg-gray-200' placeholder='Email' />
-        <input type='text' className='bg-gray-200' placeholder='Email' />
+      <form
+        className='py-44'
+        onClick={() => {
+          onSubmit();
+        }}
+      >
+        <input
+          type='email'
+          className='bg-gray-200'
+          placeholder='Email'
+          ref={email}
+        />
+        <input
+          type='password'
+          className='bg-gray-200'
+          placeholder='Password'
+          ref={password}
+        />
         <button>Submit</button>
-      </div>
+      </form>
     </div>
   );
 }
