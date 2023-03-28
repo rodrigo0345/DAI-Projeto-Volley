fc4e40b03a03f19175475735eef8a43412499510
Rodrigo Casanova <rodrigocralha@gmail.com>
2023-03-28 20:28:54 +0100

    Adding side bar to the dashboard

--- frontend/views/DashboardView.tsx
+++ frontend/views/DashboardView.tsx
@@ -1,11 +1,77 @@
 import RegisterRequest from 'Frontend/generated/com/example/application/controller/Auth/RegisterRequest';
-import { useRef } from 'react';
+import { useContext, useRef, useState } from 'react';
+import SidePanel, {
+  AsideContent,
+} from 'Frontend/components/sidePanel/SidePanel';
+import { UserContext } from 'Frontend/contexts/UserContext';
+import { AiOutlineDashboard } from 'react-icons/ai';
+import { BsCalendarDate } from 'react-icons/bs';
+
+enum Menu {
+  DASHBOARD = 'DASHBOARD',
+  CALENDAR = 'CALENDAR',
+}
 
 export default function DashboardView() {
+  const { user, logout } = useContext(UserContext);
+  const [menu, setMenu] = useState<Menu>(Menu.DASHBOARD);
+
+  const content: AsideContent<Menu>[] = [
+    {
+      id: 0,
+      text: 'Dashboard',
+      icon: (
+        <AiOutlineDashboard
+          color={menu === Menu.DASHBOARD ? 'white' : 'black'}
+          size={20}
+        ></AiOutlineDashboard>
+      ),
+      activator: {
+        setter: setMenu,
+        state: menu,
+      },
+      targetState: Menu.DASHBOARD,
+      link: '/dashboard',
+    },
+    {
+      id: 1,
+      text: 'Calendário',
+      icon: (
+        <BsCalendarDate
+          color={menu === Menu.CALENDAR ? 'white' : 'black'}
+          size={20}
+        ></BsCalendarDate>
+      ),
+      activator: {
+        setter: setMenu,
+        state: menu,
+      },
+      targetState: Menu.CALENDAR,
+      link: '/calendar',
+    },
+    // adicionar novos menus é aqui
+  ];
+
   return (
-    <main className='min-h-screen pt-28'>
-      <div className='flex flex-col items-start justify-center'>
-        <h1 className='text-4xl font-bold'>Dashboard</h1>
+    <main className='min-h-screen flex'>
+      <SidePanel user={user} logout={logout} content={content}></SidePanel>
+      <div>
+        {menu === Menu.DASHBOARD && (
+          <div className='flex flex-col gap-4 pt-28'>
+            <h1 className='text-3xl font-bold'>Dashboard</h1>
+            <p className='text-lg'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            </p>
+          </div>
+        )}
+        {menu === Menu.CALENDAR && (
+          <div className='flex flex-col gap-4 pt-28'>
+            <h1 className='text-3xl font-bold'>Calendário</h1>
+            <p className='text-lg'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            </p>
+          </div>
+        )}
       </div>
     </main>
   );
