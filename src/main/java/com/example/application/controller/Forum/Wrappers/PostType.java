018c7c8923e532218ba3a6e730d6c10db18060ee
Rodrigo Casanova <rodrigocralha@gmail.com>
2023-04-14 08:54:50 +0100

    forgot some files

---
+++ src/main/java/com/example/application/controller/Forum/Wrappers/PostType.java
@@ -0,0 +1,16 @@
+package com.example.application.controller.Forum.Wrappers;
+
+import java.util.concurrent.Callable;
+
+import com.example.application.model.News;
+import com.example.application.model.Ride;
+
+public class PostType {
+
+    public News news;
+    public Ride ride;
+
+    public PostType() {
+    }
+
+}
