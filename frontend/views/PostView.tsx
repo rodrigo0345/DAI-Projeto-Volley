907f0bfb36c97083ec079275bb3e0b89cdddf5fe
Rodrigo Casanova <rodrigocralha@gmail.com>
2023-04-17 12:11:06 +0100

    saving

--- frontend/views/PostView.tsx
+++ frontend/views/PostView.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 export default function PostView() {
   const { id } = useParams<{ id: string }>();
+
+  useEffect(() => {
+    (async () => {
+      await addClick(id);
+    })();
+  }, []);
+
   return <div>PostView - {id}</div>;
 }
