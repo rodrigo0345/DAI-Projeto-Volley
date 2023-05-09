1c14b49219606407cb1c23cfa79c6070c155b0dc
Ifini <david.eira.sousa@gmail.com>
2023-05-09 22:48:43 +0100

    PracticeRepository 2.0 :)

---
+++ src/main/java/com/example/application/repository/PracticeRepository.java
@@ -0,0 +1,18 @@
+package com.example.application.repository;
+
+import java.util.List;
+
+import javax.persistence.Table;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.application.model.Practice;
+
+@Table(name = "news")
+public interface PracticeRepository extends CrudRepository<Practice,Integer> {
+    
+    public List<Practice> findAll(Pageable rq);
+
+    public Practice findById(Long Id);
+}
