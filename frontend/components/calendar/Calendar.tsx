08da8aaf6c9ff55549e4618ee23b7ae950edd41d
Rodrigo Casanova <rodrigocralha@gmail.com>
2023-05-11 19:28:40 +0100

    calendar service

--- frontend/components/calendar/Calendar.tsx
+++ frontend/components/calendar/Calendar.tsx
@@ -3,15 +3,13 @@ import { Group, Indicator } from '@mantine/core';
 import { Calendar } from '@mantine/dates';
 import dayjs from 'dayjs';
 import { getAllEvents } from 'Frontend/generated/CalendarController';
-import CalendarEvent from 'Frontend/generated/com/example/application/model/CalendarEvent';
 import { format } from 'date-fns';
 import { motion } from 'framer-motion';
+import Event from 'Frontend/generated/com/example/application/service/CalendarService/Event';
 
 export default function CalendarView() {
-  const [events, setEvents] = useState<
-    (CalendarEvent | undefined)[] | undefined
-  >([]);
-  const [showEvent, setShowEvent] = useState<CalendarEvent[] | undefined>(
+  const [events, setEvents] = useState<(Event | undefined)[] | undefined>([]);
+  const [showEvent, setShowEvent] = useState<(Event | undefined)[] | undefined>(
     undefined
   );
   const [selected, setSelected] = useState<Date[]>([]);
@@ -29,14 +27,12 @@ export default function CalendarView() {
     if (selected.length === 1) {
       const selectedDate = selected[0];
       const selectedEvents = events?.filter((event) => {
-        if (
-          dayjs(selectedDate).isSame(new Date(event?.startDate ?? ''), 'date')
-        ) {
+        if (dayjs(selectedDate).isSame(new Date(event?.date ?? ''), 'date')) {
           return true;
         }
         return false;
       });
-      setShowEvent(selectedEvents as CalendarEvent[]);
+      setShowEvent(selectedEvents as Event[]);
     } else {
       setShowEvent(undefined);
     }
@@ -62,7 +58,7 @@ export default function CalendarView() {
           const day = date.getDate();
           console.log({ events });
           const isInEvents = events?.find((event) => {
-            if (dayjs(date).isSame(new Date(event?.startDate ?? ''), 'date')) {
+            if (dayjs(date).isSame(new Date(event?.date ?? ''), 'date')) {
               return true;
             }
             return false;
@@ -91,7 +87,7 @@ export default function CalendarView() {
                   layout
                   className='flex py-4'
                   onClick={() => {
-                    window.location.href = event.linkToPost ?? '#';
+                    window.location.href = event?.url ?? '#';
                   }}
                 >
                   <div className='mr-4 flex-1'>
@@ -99,12 +95,10 @@ export default function CalendarView() {
                       {event?.title}
                     </h4>
                     <div className='mt-1 text-sm text-gray-400'>
-                      <span>{event?.description}</span> •{' '}
+                      <span>No description</span> •{' '}
                       <time>
-                        {format(
-                          new Date(event?.startDate ?? ''),
-                          'dd/mm/yyyy'
-                        ) ?? ''}
+                        {format(new Date(event?.date ?? ''), 'dd/mm/yyyy') ??
+                          ''}
                       </time>
                     </div>
                   </div>
