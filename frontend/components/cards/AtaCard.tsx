777e6029b8551479350ebb99eb143028aeb914ec
Rodrigo Casanova <rodrigocralha@gmail.com>
2023-05-29 19:29:18 +0100

    atas working

--- frontend/components/cards/AtaCard.tsx
+++ frontend/components/cards/AtaCard.tsx
@@ -6,6 +6,8 @@ import { AiOutlineDelete } from 'react-icons/ai';
 import { BsDownload } from 'react-icons/bs';
 import Report from 'Frontend/generated/com/example/application/model/Report';
 import Ata from 'Frontend/generated/com/example/application/model/Ata';
+import ModalBox from '../modalBox/ModalBox';
+import { format } from 'date-fns';
 
 export default function AtaCard({
   user,
@@ -16,8 +18,14 @@ export default function AtaCard({
   user: LoginUser | undefined;
   onDelete?: (id: number) => void;
 }) {
+  const [open, setOpen] = React.useState(false);
   return (
-    <div className='cursor-pointer w-60 h-60 overflow-hidden rounded-lg p-2 flex-none py-6 px-3 first:pl-6 last:pr-6 bg-yellow-100 mb-2 mt-2'>
+    <div
+      onClick={() => {
+        setOpen(!open);
+      }}
+      className='cursor-pointer w-60 h-60 overflow-hidden rounded-lg p-2 flex-none py-6 px-3 first:pl-6 last:pr-6 bg-yellow-100 mb-2 mt-2'
+    >
       <div className='flex justify-between'>
         <h1 className='text-xl m-1'>{ataSubject?.title}</h1>
         <span className='h-full flex items-center justify-center'>
@@ -29,6 +37,21 @@ export default function AtaCard({
           </button>
         </span>
       </div>
+      <p className='text-ellipsis overflow-hidden'>{ataSubject?.summary}</p>
+      <ModalBox
+        openModal={open}
+        setOpenModal={setOpen}
+        key={0}
+        title={ataSubject?.title}
+      >
+        <div className='flex flex-col'>
+          <h3 className='text-lg text-gray-400'>
+            Ata criada a
+            {' ' + format(new Date(ataSubject?.createdAt ?? 0), 'dd/MM/yyyy')}
+          </h3>
+          <p>{ataSubject?.summary}</p>
+        </div>
+      </ModalBox>
     </div>
   );
 }
